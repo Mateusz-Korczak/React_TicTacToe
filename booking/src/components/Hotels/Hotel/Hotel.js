156e4a8498df2c6d21b7217ea3018532ec2a4c42
Mateusz Korczak <mkorczak.poczta@gmail.com>
2023-01-05 12:10:48 +0100

    Created booking page basics

--- booking/src/components/Hotels/Hotel/Hotel.js
+++ booking/src/components/Hotels/Hotel/Hotel.js
@@ -1,11 +1,51 @@
-import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './Hotel.module.css';
+import hotelImg from '../../../assets/images/hotel.jpg'; 
 
-function Hotel() {
+
+const propTypes = {
+  name: PropTypes.string.isRequired,
+  city: PropTypes.string.isRequired,
+  rating: PropTypes.number.isRequired,
+  description: PropTypes.string.isRequired
+};
+function Hotel(props) {
   return (
-    <div>
-      hotel
+    <div className={`card ${styles.hotel}`}>
+      <div className="card-body">
+      
+        <div className="row">
+          <div className="col-4">
+            <img
+              src={hotelImg}
+              alt=""
+              className="img-fluid img-thumbnail" />
+          </div>
+          <div className="col-8">
+            <div className="row">
+              <div className="col">
+                <p className={styles.title}>{props.name}</p>
+                <span className="badge badge-light">{props.city}</span>
+              </div>
+              <div className="col text-right">
+                <h5>Ocena: {props.rating}</h5>
+                <a href="#" className="btn btn-primary mt-2 px-4">Pokaż</a>
+              </div>
+            </div>
+          </div>
+          
+          <div className="col-12">
+            <p className={styles.description}>
+              {props.description}
+            </p>
+          </div>
+        </div>
+
+      </div>
     </div>
   );
 }
+
+Hotel.propTypes = propTypes;
 
 export default Hotel;
